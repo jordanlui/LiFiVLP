e12d68862b53ad58fce900a02f603465d689eb53
jordan.lui@gmail.com <jordan.lui@gmail.com>
2018-04-22 21:47:48 -0700

    Added support for threshold Gaussian values, which is useful in sensor saturation cases

--- fitGaussian.m
+++ fitGaussian.m
@@ -1,9 +1,11 @@
-function [model,x,y,z] = fitGaussian(x,y,z,k,symmetric)
+function [model,x,y,z] = fitGaussian(x,y,z,k,symmetric,threshold)
     % Fit a 2D Gaussian
     % Gaussian Function
     posTol = 800; % Tolerance for spatial value
+    sigmaTol = 1000; % Tolerance for size of sigma value
     sigma0 = 300; % Guess for Gaussian width
-    sigmaTol = 1000;
+    threshTol = 2; % Max tolerance for Gaussian cutoff height
+    threshold0 = 0.3;
     
     if nargin < 5
         % If not specified, we will fit an asymmetric Gaussian
@@ -14,21 +16,33 @@ function [model,x,y,z] = fitGaussian(x,y,z,k,symmetric)
     [xi,yi,zi,x,y,z] = prepGaussian(x,y,z,k);
     % Refine starting point estimate by looking at max z value
     [amp, ind] = max(z);
+    % Set tolerances
+    x0 = [amp x(ind) y(ind) sigma0 sigma0]; % Guess. [a x0 y0 sx sy]
+    xU = [5 posTol posTol sigmaTol sigmaTol];
+    xL = [0 -posTol -posTol 0 0];
     
     if symmetric
-        gaussFun = @(a,x0,y0,sx,x,y) a .* exp(-( (x-x0).^2./2./sx^2 + (y-y0).^2./2./sx^2 ));
-        x0 = [amp x(ind) y(ind) sigma0]; % Guess. [a x0 y0 sx sy]
-        xU = [5 posTol posTol sigmaTol];
-        xL = [0 -posTol -posTol 0];
+        f = @(a,x0,y0,sx,x,y) a .* exp(-( (x-x0).^2./2./sx^2 + (y-y0).^2./2./sx^2 ));
+        x0 = x0(1:4);
+        xU = xU(1:4);
+        xL = xL(1:4);
     else
-        gaussFun = @(a,x0,y0,sx,sy,x,y) a .* exp(-( (x-x0).^2./2./sx^2 + (y-y0).^2./2./sy^2 ));
-        x0 = [amp x(ind) y(ind) sigma0 sigma0]; % Guess. [a x0 y0 sx sy]
-        xU = [5 posTol posTol sigmaTol sigmaTol];
-        xL = [0 -posTol -posTol 0 0];
+        f = @(a,x0,y0,sx,sy,x,y) a .* exp(-( (x-x0).^2./2./sx^2 + (y-y0).^2./2./sy^2 ));
+        
+    end
+    
+    if threshold
+        if threshold ~=1 % If specific guess was supplied, load it
+            threshold0 = threshold;
+        end
+        f = @(a,x0,y0,sx,sy,threshold,x,y) gaussFun(a,x0,y0,sx,sy,x,y,threshold);
+        x0 = [x0 threshold0];
+        xU = [xU threshTol];
+        xL = [xL 0];
     end
     
     % Fit the model
-    model = fit([x y],z,gaussFun,'Start',x0,...
+    model = fit([x y],z,f,'Start',x0,...
         'Lower',xL,...
         'Upper',xU);
     
--- gaussFun.m
+++ gaussFun.m
@@ -1,5 +1,15 @@
-function z = gaussFun(a,x0,y0,sx,sy,x,y)
+function z = gaussFun(a,x0,y0,sx,sy,x,y,threshold)
     % Calculate value of a 2D gaussian
+   
+    if nargin < 8
+        threshold = 0;
+    end
     
     z = a.*exp(-((x-x0).^2./2./sx^2+(y-y0).^2./2./sy^2));
+    
+    % Use following logic to flatten top of Gaussian as desired
+    if threshold
+        ind = z>threshold;
+        z(ind) = threshold;
+    end
 end
